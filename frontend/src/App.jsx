98017fd6544d9e44118b52e54a41b3a87036f5ca
Nicole Weimar <nweimar@g.ucla.edu>
2026-05-17 15:30:46 -0700

    Add posts frontend integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import PostBoard from "./PostBoard";
 import './App.css'
 
 function App() {
@@ -114,6 +115,7 @@ function App() {
       </section>
 
       <div className="ticks"></div>
+      <PostBoard />
       <section id="spacer"></section>
     </>
   )
