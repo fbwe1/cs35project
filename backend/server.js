c4107b7f081cd93ef71bbe18fe408101ac3e0fff
Nicole Weimar <nweimar@g.ucla.edu>
2026-05-17 15:24:29 -0700

    Add posts backend and frontend integration

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const cors = require("cors");
+
 const app = express();
 const port = 3001;
-//similar to the node.js tutorial as a skeleton
+
+app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("UCLAway backend is running");
-  });
+});
 
-//import and mount post routes
 const postRoutes = require("./routes/posts");
 app.use("/posts", postRoutes);
 
 app.listen(port, () => {
-    console.log('Server is running on port 3001'); // testing
+    console.log('Server is running on port 3001');
 });
-
